5c9629d7d4d04efa8ca35bd803064c0db111a3fc
Burak Emir <burak.emir@gmail.com>
2022-06-10 16:33:07 +0200

    use .toml for config

--- astcogen/astcogen.go
+++ astcogen/astcogen.go
@@ -9,7 +9,20 @@ import (
 	"go/token"
 	"log"
 	"os"
+	"path"
+
+	toml "github.com/pelletier/go-toml/v2"
 )
+
+// config is a struct that supplies parameters to the run of the astcogen tool.
+// The config is a .toml file with the format
+//   input = "<path to input file>"
+//   output = "<path to output file>"
+// Paths are interpreted as relative to the config file.
+type Config struct {
+	Input  string
+	Output string
+}
 
 type Visitor struct {
 	fset *token.FileSet
@@ -39,8 +52,23 @@ func (v *Visitor) Visit(n ast.Node) ast.Visitor {
 }
 
 func main() {
+	if len(os.Args) != 2 {
+		log.Fatal("Need a .toml file as argument.")
+	}
+	configFilePath := os.Args[1]
+	configDir := path.Dir(configFilePath)
+	configFile, err := os.ReadFile(configFilePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var cfg Config
+	err = toml.Unmarshal(configFile, &cfg)
+	if err != nil {
+		panic(err)
+	}
+
 	fset := token.NewFileSet()
-	file, err := parser.ParseFile(fset, "example/hello.go", nil, parser.AllErrors)
+	file, err := parser.ParseFile(fset, path.Join(configDir, cfg.Input), nil, parser.AllErrors)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -48,5 +76,10 @@ func main() {
 	visitor := &Visitor{fset: fset}
 	ast.Walk(visitor, file)
 
-	printer.Fprint(os.Stdout, fset, file)
+	outputFile, err := os.Create(path.Join(configDir, cfg.Output))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer outputFile.Close()
+	printer.Fprint(outputFile, fset, file)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/burakemir/astcogen-go
 
 go 1.18
+
+require github.com/pelletier/go-toml/v2 v2.0.2 // indirect
